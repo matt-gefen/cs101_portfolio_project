3f660706d916f7ad8879d2743d50deeffae2a3a2
matt-gefen <matt.gefen@gmail.com>
2021-07-25 13:12:44 -0600

    initialized drawings dictionary

---
+++ tarokka.py
@@ -0,0 +1,16 @@
+drawings_player = {
+    1:'This card tells of history. Knowledge of the ancient will help you better understand your enemy.',
+    2:'This card tells of a powerful force for good and protection, a holy symbol of great hope.',
+    3:'This is a card of power and strength. It tells of a weapon of vengeance: a sword of sunlight.',
+    4:'This card sheds light on one who will help you greatly in the battle against darkness.',
+    5:'Your enemy is a creature of darkness, whose powers are beyond mortality. This card will leadyou to him!'}
+print(drawings_player)
+
+# drawings_dm = {
+#     1:
+#     2:
+#     3:
+#     4:
+#     5:
+# }
+# high_deck = {'Artifact':'The importance of some physical object that must be obtained, protected, or destroyed at all costs'}
