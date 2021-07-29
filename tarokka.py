ff1db98050ef26e769465b1515165da956ce52dc
matt-gefen <matt.gefen@gmail.com>
2021-07-29 16:47:59 -0600

    will error - adding locations to common_deck pt1

--- tarokka.py
+++ tarokka.py
@@ -1,133 +1,202 @@
 common_deck = {
     1: {
         'name' : 'Warrior', 
-        'represents' : 'Strength and force personified—violence—those who use force to accomplish their goals'},
+        'represents' : 'Strength and force personified—violence—those who use force to accomplish their goals',
+        'pred_location':'That which you seek lies in the womb of darkness, the devil’s lair: the one place to which he must return.',
+        'dm_location':'' },
     2: {
         'name' : 'Avenger',
-        'represents' : 'Justice and revenge for great wrongs—those on a quest to rid the world of great evil'},
+        'represents' : 'Justice and revenge for great wrongs—those on a quest to rid the world of great evil',
+        'pred_location':'The treasure lies in a dragon’s house, in hands once clean and now corrupted.',
+        'dm_location':'The treasure is in the possession of Vladimir Horngaard in Argynvostholt (chapter 7, area Q36).'},
     3: {
         'name' : 'Paladin',
-        'represents' : 'Just and noble warriors—those who live by a code of honor and integrity'},
+        'represents' : 'Just and noble warriors—those who live by a code of honor and integrity',
+        'pred_location':'I see a sleeping prince, a servant of light and the brother of darkness. The treasure lies with him.',
+        'dm_location':'The treasure lies in Sergei’s tomb (chapter 4, area K85).'},
     4: {
         'name': 'Soldier',
-        'represents':'War and sacrifice—the stamina to endure great hardship'},
+        'represents':'War and sacrifice—the stamina to endure great hardship',
+        'pred_location':'Go to the mountains. Climb the white tower guarded by golden knights.',
+        'dm_location':'The treasure lies on the rooftop of the Tsolenka Pass guard tower (chapter 9, area T6).'},
     5: {
         'name': 'Mercenary',
-        'represents': 'Inner strength and fortitude—those who fight for power or wealth'},
+        'represents': 'Inner strength and fortitude—those who fight for power or wealth',
+        'pred_location':'The thing you seek lies with the dead, under mountains of gold coins.',
+        'dm_location':'The treasure lies in a crypt in Castle Ravenloft (chapter 4, area K84, crypt 31).'},
     6: {
         'name': 'Myrmidion',
-        'represents': 'Great heroes—a sudden reversal of fate—the triumph of the underdog over a mighty enemy'},
+        'represents': 'Great heroes—a sudden reversal of fate—the triumph of the underdog over a mighty enemy',
+        'pred_location':'Look for a den of wolves in the hills overlooking a mountain lake. The treasure belongs to Mother Night.',
+        'dm_location': 'The treasure lies in the shrine of Mother Night in the werewolf den (chapter 15, area Z7).'},
     7: {
         'name': 'Berserker',
-        'represents': 'The brutal and barbaric side of warfare—bloodlust—those with a bestial nature'},
+        'represents': 'The brutal and barbaric side of warfare—bloodlust—those with a bestial nature',
+        'pred_location':'Find the Mad Dog’s crypt. The treasure lies within, beneath blackened bones.',
+        'dm_location': 'The treasure lies in the crypt of General Kroval “Mad Dog” Grislek (chapter 4, area K84, crypt 38).'},
     8: {
         'name': 'Hooded One',
-        'represents': 'Bigotry, intolerance, and xenophobia—a mysterious presence or newcomer'},
+        'represents': 'Bigotry, intolerance, and xenophobia—a mysterious presence or newcomer',
+        'pred_location':,
+        'dm_location': 'The treasure is inside the head of the giant statue in the Amber Temple (chapter 13, area X5a).'},
     9: {
         'name': 'Dictator',
-        'represents': 'All that is wrong with government and leadership—those who rule through fear and violence'},
+        'represents': 'All that is wrong with government and leadership—those who rule through fear and violence',
+        'pred_location':,
+        'dm_location': ''},
     10:{
         'name': 'Torturer',
-        'represents':'The coming of suffering or merciless cruelty—one who is irredeemably evil or sadistic'},
+        'represents':'The coming of suffering or merciless cruelty—one who is irredeemably evil or sadistic',
+        'pred_location':,
+        'dm_location': ''},
     11:{
         'name': 'Wizard', 
-        'represents':'Mystery and riddles—the unknown—those who crave magical power and great knowledge'},
+        'represents':'Mystery and riddles—the unknown—those who crave magical power and great knowledge',
+        'pred_location':,
+        'dm_location': ''},
     12:{
         'name': 'Transmuter', 
-        'represents':'A new discovery—the coming of unexpected things—unforeseen consequences and chaos'},
+        'represents':'A new discovery—the coming of unexpected things—unforeseen consequences and chaos',
+        'pred_location':,
+        'dm_location': ''},
     13:{
         'name': 'Diviner', 
-        'represents':'The pursuit of knowledge tempered by wisdom—truth and honesty—sages and prophecy'},
+        'represents':'The pursuit of knowledge tempered by wisdom—truth and honesty—sages and prophecy',
+        'pred_location':,
+        'dm_location': ''},
     14:{
         'name': 'Enchanter', 
-        'represents':'Inner turmoil that comes from confusion, fear of failure, or false information'},
+        'represents':'Inner turmoil that comes from confusion, fear of failure, or false information',
+        'pred_location':,
+        'dm_location': ''},
     15:{
         'name': 'Elementalist', 
-        'represents':'The triumph of nature over civilization—natural disasters and bountiful harvests'},
+        'represents':'The triumph of nature over civilization—natural disasters and bountiful harvests',
+        'pred_location':,
+        'dm_location': ''},
     17:{
         'name': 'Evoker', 
-        'represents':'Magical or supernatural power that can\'t be controlled—magic for destructive ends'},
+        'represents':'Magical or supernatural power that can\'t be controlled—magic for destructive ends',
+        'pred_location':,
+        'dm_location': ''},
     18:{
         'name': 'Illusionist', 
-        'represents':'Lies and deceit—grand conspiracies—secret societies—the presence of a dupe or a saboteur'},
+        'represents':'Lies and deceit—grand conspiracies—secret societies—the presence of a dupe or a saboteur',
+        'pred_location':,
+        'dm_location': ''},
     19:{
         'name': 'Necromancer', 
-        'represents':'Unnatural events and unhealthy obsessions—those who follow a destructive path'},
+        'represents':'Unnatural events and unhealthy obsessions—those who follow a destructive path',
+        'pred_location':,
+        'dm_location': ''},
     20:{
         'name': 'Conjurer', 
-        'represents':'The coming of an unexpected supernatural threat—those who think of themselves as gods'},
+        'represents':'The coming of an unexpected supernatural threat—those who think of themselves as gods',
+        'pred_location':,
+        'dm_location': ''},
     21:{
         'name': 'Rogue', 
-        'represents':'Anyone for whom money is important—those who believe money is the key to their success'},
+        'represents':'Anyone for whom money is important—those who believe money is the key to their success',
+        'pred_location':,
+        'dm_location': ''},
     22:{
         'name': 'Swashbuckler', 
-        'represents':'Those who like money yet give it up freely—likable rogues and rapscallions'},
+        'represents':'Those who like money yet give it up freely—likable rogues and rapscallions',
+        'pred_location':,
+        'dm_location': ''},
     23:{
         'name': 'Philanthropist', 
-        'represents':'Charity and giving on a grand scale—those who use wealth to fight evil and sickness'},
+        'represents':'Charity and giving on a grand scale—those who use wealth to fight evil and sickness',
+        'pred_location':,
+        'dm_location': ''},
     24:{
         'name': 'Trader', 
-        'represents':'Commerce—smuggling and black markets—fair and equitable trades'},
+        'represents':'Commerce—smuggling and black markets—fair and equitable trades',
+        'pred_location':,
+        'dm_location': ''},
     25:{
         'name': 'Merchant', 
-        'represents':'A rare commodity or business opportunity—deceitful or dangerous business transactions'},
+        'represents':'A rare commodity or business opportunity—deceitful or dangerous business transactions',
+        'pred_location':,
+        'dm_location': ''},
     26:{
         'name': 'Guild Member', 
-        'represents':'Like-minded individuals joined together in a common goal—pride in one\'s work'},
+        'represents':'Like-minded individuals joined together in a common goal—pride in one\'s work',
+        'pred_location':,
+        'dm_location': ''},
     27:{
         'name': 'Beggar', 
-        'represents':'Sudden change in economic status or fortune'},
+        'represents':'Sudden change in economic status or fortune',
+        'pred_location':},
+        'dm_location': '',
     28:{
         'name': 'Thief', 
-        'represents':'Those who steal or burgle—a loss of property, beauty, innocence, friendship, or reputation'},
+        'represents':'Those who steal or burgle—a loss of property, beauty, innocence, friendship, or reputation',
+        'pred_location':},
     29:{
         'name': 'Tax Collector', 
-        'represents':'Corruption—honesty in an otherwise corrupt government or organization'},
+        'represents':'Corruption—honesty in an otherwise corrupt government or organization',
+        'pred_location':},
     30:{
         'name': 'Miser', 
-        'represents':'Hoarded wealth—those who are irreversibly unhappy or who think money is meaningless'},
+        'represents':'Hoarded wealth—those who are irreversibly unhappy or who think money is meaningless',
+        'pred_location':},
     31:{
         'name': 'Priest', 
-        'represents':'Enlightenment—those who follow a deity, a system of values, or a higher purpose'},
+        'represents':'Enlightenment—those who follow a deity, a system of values, or a higher purpose',
+        'pred_location':},
     32:{
         'name': 'Monk', 
-        'represents':'Serenity—inner strength and self-reliance—supreme confidence bereft of arrogance'},
+        'represents':'Serenity—inner strength and self-reliance—supreme confidence bereft of arrogance',
+        'pred_location':},
     33:{
         'name': 'Missionary', 
-        'represents':'Those who spread wisdom and faith to others—warnings of the spread of fear and ignorance'},
+        'represents':'Those who spread wisdom and faith to others—warnings of the spread of fear and ignorance',
+        'pred_location':},
     34:{
         'name': 'Healer', 
-        'represents':'Healing—a contagious illness, disease, or curse—those who practice the healing arts'},
+        'represents':'Healing—a contagious illness, disease, or curse—those who practice the healing arts',
+        'pred_location':},
     35:{
         'name': 'Shepherd', 
-        'represents':'Those who protect others—one who bears a burden far too great to be shouldered alone'},
+        'represents':'Those who protect others—one who bears a burden far too great to be shouldered alone',
+        'pred_location':},
     36:{
         'name': 'Druid', 
-        'represents':'The ambivalence and cruelty of nature and those who feel drawn to it—inner turmoil'},
+        'represents':'The ambivalence and cruelty of nature and those who feel drawn to it—inner turmoil',
+        'pred_location':},
     37:{
         'name': 'Anarchist', 
-        'represents':'A fundamental change brought on by one whose beliefs are being put to the test'},
+        'represents':'A fundamental change brought on by one whose beliefs are being put to the test',
+        'pred_location':},
     38:{
         'name': 'Charlatan', 
-        'represents':'Liars—those who profess to believe one thing but actually believe another'},
+        'represents':'Liars—those who profess to believe one thing but actually believe another',
+        'pred_location':},
     39:{
         'name': 'Bishop', 
-        'represents':'Strict adherence to a code or a belief—those who plot, plan, and scheme'},
+        'represents':'Strict adherence to a code or a belief—those who plot, plan, and scheme',
+        'pred_location':},
     40:{
         'name': 'Traitor', 
-        'represents':'Betrayal by someone close and trusted—a weakening or loss of faith'}
+        'represents':'Betrayal by someone close and trusted—a weakening or loss of faith',
+        'pred_location':}
     }
 
-high_deck = {}
+high_deck = {
+    1:{},
+    2:{},
+    3:{}
+}
 # {
 #     'Artifact':'The importance of some physical object that must be obtained, protected, or destroyed at all costs'}
 
 drawings= {
-    1:['This card tells of history. Knowledge of the ancient will help you better understand your enemy.','The Tome of Strahd', common_deck],
-    2:['This card tells of a powerful force for good and protection, a holy symbol of great hope.','The Holy Symbol of Ravenkind', common_deck],
-    3:['This is a card of power and strength. It tells of a weapon of vengeance: a sword of sunlight.','The Sunsword', common_deck],
-    4:['This card sheds light on one who will help you greatly in the battle against darkness.','Strahd\'s Enemy', high_deck],
-    5:['Your enemy is a creature of darkness, whose powers are beyond mortality. This card will lead you to him!','Strahd\'s Ravenloft Location', high_deck]}
+    1:{'reading': 'This card tells of history. Knowledge of the ancient will help you better understand your enemy.', 'name': 'The Tome of Strahd'},
+    2:{'reading': 'This card tells of a powerful force for good and protection, a holy symbol of great hope.','name': 'The Holy Symbol of Ravenkind'},
+    3:{'reading': 'This is a card of power and strength. It tells of a weapon of vengeance: a sword of sunlight.','name':'The Sunsword'},
+    4:{'reading': 'This card sheds light on one who will help you greatly in the battle against darkness.','name': 'Strahd\'s Enemy'},
+    5:{'reading':'Your enemy is a creature of darkness, whose powers are beyond mortality. This card will lead you to him!','name':'Strahd\'s Ravenloft pred_location'}}
 
 # testing the output of the player-side explainations of each card in the reading.
 for i in range(1,6):
