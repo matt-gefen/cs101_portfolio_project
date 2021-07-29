2b4c08636222c7bb0f6b91668b838df944f15f07
matt-gefen <matt.gefen@gmail.com>
2021-07-29 16:19:32 -0600

    adjusted low deck to be dicts of dicts

--- tarokka.py
+++ tarokka.py
@@ -1,44 +1,121 @@
 common_deck = {
-    'Warrior': ['Strength and force personified—violence—those who use force to accomplish their goals'],
-    'Avenger':['Justice and revenge for great wrongs—those on a quest to rid the world of great evil'],
-    'Paladin':['Just and noble warriors—those who live by a code of honor and integrity'],
-    'Soldier':['War and sacrifice—the stamina to endure great hardship'],
-    'Mercenary':['Inner strength and fortitude—those who fight for power or wealth'],
-    'Myrmidion':['Great heroes—a sudden reversal of fate—the triumph of the underdog over a mighty enemy'],
-    'Berserker':['The brutal and barbaric side of warfare—bloodlust—those with a bestial nature'],
-    'Hooded One':['Bigotry, intolerance, and xenophobia—a mysterious presence or newcomer'],
-    'Dictator':['All that is wrong with government and leadership—those who rule through fear and violence'],
-    'Torturer':['The coming of suffering or merciless cruelty—one who is irredeemably evil or sadistic'],
-    'Wizard':['Mystery and riddles—the unknown—those who crave magical power and great knowledge'],
-    'Transmuter':['A new discovery—the coming of unexpected things—unforeseen consequences and chaos'],
-    'Diviner':['The pursuit of knowledge tempered by wisdom—truth and honesty—sages and prophecy'],
-    'Enchanter':['Inner turmoil that comes from confusion, fear of failure, or false information'],
-    'Abjurer':['Those guided by logic and reasoning—warns of an overlooked clue or piece of information'],
-    'Elementalist':['The triumph of nature over civilization—natural disasters and bountiful harvests'],
-    'Evoker':['Magical or supernatural power that can\'t be controlled—magic for destructive ends'],
-    'Illusionist':['Lies and deceit—grand conspiracies—secret societies—the presence of a dupe or a saboteur'],
-    'Necromancer':['Unnatural events and unhealthy obsessions—those who follow a destructive path'],
-    'Conjurer':['The coming of an unexpected supernatural threat—those who think of themselves as gods'],
-    'Rogue':['Anyone for whom money is important—those who believe money is the key to their success'],
-    'Swashbuckler':['Those who like money yet give it up freely—likable rogues and rapscallions'],
-    'Philanthropist':['Charity and giving on a grand scale—those who use wealth to fight evil and sickness'],
-    'Trader':['Commerce—smuggling and black markets—fair and equitable trades'],
-    'Merchant':['A rare commodity or business opportunity—deceitful or dangerous business transactions'],
-    'Guild Member':['Like-minded individuals joined together in a common goal—pride in one\'s work'],
-    'Beggar':['Sudden change in economic status or fortune'],
-    'Thief':['Those who steal or burgle—a loss of property, beauty, innocence, friendship, or reputation'],
-    'Tax Collector':['Corruption—honesty in an otherwise corrupt government or organization'],
-    'Miser':['Hoarded wealth—those who are irreversibly unhappy or who think money is meaningless'],
-    'Priest':['Enlightenment—those who follow a deity, a system of values, or a higher purpose'],
-    'Monk':['Serenity—inner strength and self-reliance—supreme confidence bereft of arrogance'],
-    'Missionary':['Those who spread wisdom and faith to others—warnings of the spread of fear and ignorance'],
-    'Healer':['Healing—a contagious illness, disease, or curse—those who practice the healing arts'],
-    'Shepherd':['Those who protect others—one who bears a burden far too great to be shouldered alone'],
-    'Druid':['The ambivalence and cruelty of nature and those who feel drawn to it—inner turmoil'],
-    'Anarchist':['A fundamental change brought on by one whose beliefs are being put to the test'],
-    'Charlatan':['Liars—those who profess to believe one thing but actually believe another'],
-    'Bishop':['Strict adherence to a code or a belief—those who plot, plan, and scheme'],
-    'Traitor':['Betrayal by someone close and trusted—a weakening or loss of faith']
+    1: {
+        'name' : 'Warrior', 
+        'represents' : 'Strength and force personified—violence—those who use force to accomplish their goals'},
+    2: {
+        'name' : 'Avenger',
+        'represents' : 'Justice and revenge for great wrongs—those on a quest to rid the world of great evil'},
+    3: {
+        'name' : 'Paladin',
+        'represents' : 'Just and noble warriors—those who live by a code of honor and integrity'},
+    4: {
+        'name': 'Soldier',
+        'represents':'War and sacrifice—the stamina to endure great hardship'},
+    5: {
+        'name': 'Mercenary',
+        'represents': 'Inner strength and fortitude—those who fight for power or wealth'},
+    6: {
+        'name': 'Myrmidion',
+        'represents': 'Great heroes—a sudden reversal of fate—the triumph of the underdog over a mighty enemy'},
+    7: {
+        'name': 'Berserker',
+        'represents': 'The brutal and barbaric side of warfare—bloodlust—those with a bestial nature'},
+    8: {
+        'name': 'Hooded One',
+        'represents': 'Bigotry, intolerance, and xenophobia—a mysterious presence or newcomer',
+    9: {
+        'name': 'Dictator',
+        'represents': 'All that is wrong with government and leadership—those who rule through fear and violence'},
+    10:{
+        'name': 'Torturer',
+        'represents':'The coming of suffering or merciless cruelty—one who is irredeemably evil or sadistic'},
+    11:{
+        'name': 'Wizard', 
+        'represents':'Mystery and riddles—the unknown—those who crave magical power and great knowledge'},
+    12:{
+        'name': 'Transmuter', 
+        'represents':'A new discovery—the coming of unexpected things—unforeseen consequences and chaos'},
+    13:{
+        'name': 'Diviner', 
+        'represents':'The pursuit of knowledge tempered by wisdom—truth and honesty—sages and prophecy'},
+    14:{
+        'name': 'Enchanter', 
+        'represents':'Inner turmoil that comes from confusion, fear of failure, or false information'},
+    15:{
+        'name': 'Elementalist', 
+        'represents':'The triumph of nature over civilization—natural disasters and bountiful harvests'},
+    17:{
+        'name': 'Evoker', 
+        'represents':'Magical or supernatural power that can\'t be controlled—magic for destructive ends'},
+    18:{
+        'name': 'Illusionist', 
+        'represents':'Lies and deceit—grand conspiracies—secret societies—the presence of a dupe or a saboteur'},
+    19:{
+        'name': 'Necromancer', 
+        'represents':'Unnatural events and unhealthy obsessions—those who follow a destructive path'},
+    20:{
+        'name': 'Conjurer', 
+        'represents':'The coming of an unexpected supernatural threat—those who think of themselves as gods'},
+    21:{
+        'name': 'Rogue', 
+        'represents':'Anyone for whom money is important—those who believe money is the key to their success'},
+    22:{
+        'name': 'Swashbuckler', 
+        'represents':'Those who like money yet give it up freely—likable rogues and rapscallions'},
+    23:{
+        'name': 'Philanthropist', 
+        'represents':'Charity and giving on a grand scale—those who use wealth to fight evil and sickness'},
+    24:{
+        'name': 'Trader', 
+        'represents':'Commerce—smuggling and black markets—fair and equitable trades'},
+    25:{
+        'name': 'Merchant', 
+        'represents':'A rare commodity or business opportunity—deceitful or dangerous business transactions'},
+    26:{
+        'name': 'Guild Member', 
+        'represents':'Like-minded individuals joined together in a common goal—pride in one\'s work'},
+    27:{
+        'name': 'Beggar', 
+        'represents':'Sudden change in economic status or fortune'},
+    28:{
+        'name': 'Thief', 
+        'represents':'Those who steal or burgle—a loss of property, beauty, innocence, friendship, or reputation'},
+    29:{
+        'name': 'Tax Collector', 
+        'represents':'Corruption—honesty in an otherwise corrupt government or organization'},
+    30:{
+        'name': 'Miser', 
+        'represents':'Hoarded wealth—those who are irreversibly unhappy or who think money is meaningless'},
+    31:{
+        'name': 'Priest', 
+        'represents':'Enlightenment—those who follow a deity, a system of values, or a higher purpose'},
+    32:{
+        'name': 'Monk', 
+        'represents':'Serenity—inner strength and self-reliance—supreme confidence bereft of arrogance'},
+    33:{
+        'name': 'Missionary', 
+        'represents':'Those who spread wisdom and faith to others—warnings of the spread of fear and ignorance'},
+    34:{
+        'name': 'Healer', 
+        'represents':'Healing—a contagious illness, disease, or curse—those who practice the healing arts'},
+    35:{
+        'name': 'Shepherd', 
+        'represents':'Those who protect others—one who bears a burden far too great to be shouldered alone'},
+    36:{
+        'name': 'Druid', 
+        'represents':'The ambivalence and cruelty of nature and those who feel drawn to it—inner turmoil'},
+    37:{
+        'name': 'Anarchist', 
+        'represents':'A fundamental change brought on by one whose beliefs are being put to the test'},
+    38:{
+        'name': 'Charlatan', 
+        'represents':'Liars—those who profess to believe one thing but actually believe another'},
+    39:{
+        'name': 'Bishop', 
+        'represents':'Strict adherence to a code or a belief—those who plot, plan, and scheme'},
+    40:{
+        'name': 'Traitor', 
+        'represents':'Betrayal by someone close and trusted—a weakening or loss of faith'}
     }
 
 high_deck = {}
