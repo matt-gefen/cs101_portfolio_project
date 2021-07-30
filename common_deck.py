10f54a5e9110700fc84a0ec4b2e95549026a872a
matt-gefen <matt.gefen@gmail.com>
2021-07-30 09:55:55 -0600

    Broke files out for modularity

---
+++ common_deck.py
@@ -0,0 +1,202 @@
+common_deck = {
+    1: {
+        'name' : 'Warrior', 
+        'represents' : 'Strength and force personified—violence—those who use force to accomplish their goals',
+        'pred_location':'That which you seek lies in the womb of darkness, the devil’s lair: the one place to which he must return.',
+        'dm_location':'The treasure lies in Strahd’s tomb (chapter 4, area K86).' },
+    2: {
+        'name' : 'Avenger',
+        'represents' : 'Justice and revenge for great wrongs—those on a quest to rid the world of great evil',
+        'pred_location':'The treasure lies in a dragon’s house, in hands once clean and now corrupted.',
+        'dm_location':'The treasure is in the possession of Vladimir Horngaard in Argynvostholt (chapter 7, area Q36).'},
+    3: {
+        'name' : 'Paladin',
+        'represents' : 'Just and noble warriors—those who live by a code of honor and integrity',
+        'pred_location':'I see a sleeping prince, a servant of light and the brother of darkness. The treasure lies with him.',
+        'dm_location':'The treasure lies in Sergei’s tomb (chapter 4, area K85).'},
+    4: {
+        'name': 'Soldier',
+        'represents':'War and sacrifice—the stamina to endure great hardship',
+        'pred_location':'Go to the mountains. Climb the white tower guarded by golden knights.',
+        'dm_location':'The treasure lies on the rooftop of the Tsolenka Pass guard tower (chapter 9, area T6).'},
+    5: {
+        'name': 'Mercenary',
+        'represents': 'Inner strength and fortitude—those who fight for power or wealth',
+        'pred_location':'The thing you seek lies with the dead, under mountains of gold coins.',
+        'dm_location':'The treasure lies in a crypt in Castle Ravenloft (chapter 4, area K84, crypt 31).'},
+    6: {
+        'name': 'Myrmidion',
+        'represents': 'Great heroes—a sudden reversal of fate—the triumph of the underdog over a mighty enemy',
+        'pred_location':'Look for a den of wolves in the hills overlooking a mountain lake. The treasure belongs to Mother Night.',
+        'dm_location': 'The treasure lies in the shrine of Mother Night in the werewolf den (chapter 15, area Z7).'},
+    7: {
+        'name': 'Berserker',
+        'represents': 'The brutal and barbaric side of warfare—bloodlust—those with a bestial nature',
+        'pred_location':'Find the Mad Dog’s crypt. The treasure lies within, beneath blackened bones.',
+        'dm_location': 'The treasure lies in the crypt of General Kroval “Mad Dog” Grislek (chapter 4, area K84, crypt 38).'},
+    8: {
+        'name': 'Hooded One',
+        'represents': 'Bigotry, intolerance, and xenophobia—a mysterious presence or newcomer',
+        'pred_location':'I see a faceless god. He awaits you at the end of a long and winding road, deep in the mountains.',
+        'dm_location': 'The treasure is inside the head of the giant statue in the Amber Temple (chapter 13, area X5a).'},
+    9: {
+        'name': 'Dictator',
+        'represents': 'All that is wrong with government and leadership—those who rule through fear and violence',
+        'pred_location':'I see a throne fit for a king.',
+        'dm_location': 'The treasure lies in Castle Ravenloft’s audience hall (chapter 4, area K25).'},
+    10:{
+        'name': 'Torturer',
+        'represents':'The coming of suffering or merciless cruelty—one who is irredeemably evil or sadistic',
+        'pred_location':'There is a town where all is not well. There you will find a house of corruption, and within, a dark room full of still ghosts.',
+        'dm_location': 'The treasure is hidden in the attic of the burgomaster’s mansion in Vallaki (chapter 5, area N3s).'},
+    11:{
+        'name': 'Wizard', 
+        'represents':'Mystery and riddles—the unknown—those who crave magical power and great knowledge',
+        'pred_location':'Look for a wizard’s tower on a lake. Let the wizard’s name and servant guide you to that which you seek.',
+        'dm_location': 'The treasure lies on the top floor of Van Richten’s Tower (chapter 11, area V7).'},
+    12:{
+        'name': 'Transmuter', 
+        'represents':'A new discovery—the coming of unexpected things—unforeseen consequences and chaos',
+        'pred_location':'Go to a place of dizzying heights, where the stone itself is alive!',
+        'dm_location': 'The treasure lies in Castle Ravenloft’s north tower peak (chapter 4, area K60).'},
+    13:{
+        'name': 'Diviner', 
+        'represents':'The pursuit of knowledge tempered by wisdom—truth and honesty—sages and prophecy',
+        'pred_location': 'Look to the one who sees all. The treasure is hidden in her camp.',
+        'dm_location': 'The treasure lies in Madam Eva’s encampment (chapter 2, area G). If she is the one performing the card reading, she says, “I think the treasure is under my very nose!”'
+        },
+    14:{
+        'name': 'Enchanter', 
+        'represents':'Inner turmoil that comes from confusion, fear of failure, or false information',
+        'pred_location':'I see a kneeling woman—a rose of great beauty plucked too soon. The master of the marsh knows of whom I speak.',
+        'dm_location':'The treasure lies under Marina’s monument in Berez (chapter 10, area U5). “The master of the marsh” refers to Burgomaster Lazlo Ulrich (area U2), whose ghost can point characters toward the monument.' },
+    15:{
+        'name': 'Elementalist', 
+        'represents':'The triumph of nature over civilization—natural disasters and bountiful harvests',
+        'pred_location':'The treasure is hidden in a small castle beneath a mountain, guarded by amber giants.',
+        'dm_location': 'The treasure is inside a model of Castle Ravenloft in the Amber Temple (chapter 13, area X20).'},
+    17:{
+        'name': 'Evoker', 
+        'represents':'Magical or supernatural power that can\'t be controlled—magic for destructive ends',
+        'pred_location':'I see a kneeling woman—a rose of great beauty plucked too soon. The master of the marsh knows of whom I speak.',
+        'dm_location': 'The treasure lies under Marina’s monument in Berez (chapter 10, area U5). “The master of the marsh” refers to Burgomaster Lazlo Ulrich (area U2), whose ghost can point characters toward the monument.'},
+    18:{
+        'name': 'Illusionist', 
+        'represents':'Lies and deceit—grand conspiracies—secret societies—the presence of a dupe or a saboteur',
+        'pred_location':'A man is not what he seems. He comes here in a carnival wagon. Therein lies what you seek.',
+        'dm_location': 'The treasure lies in Rictavio’s carnival wagon (chapter 5, area N5).'},
+    19:{
+        'name': 'Necromancer', 
+        'represents':'Unnatural events and unhealthy obsessions—those who follow a destructive path',
+        'pred_location':'A woman hangs above a roaring fire. Find her, and you will find the treasure.',
+        'dm_location': 'The treasure lies in Castle Ravenloft’s study (chapter 4, area K37).'},
+    20:{
+        'name': 'Conjurer', 
+        'represents':'The coming of an unexpected supernatural threat—those who think of themselves as gods',
+        'pred_location':'I see a dead village, drowned by a river, ruled by one who has brought great evil into the world.',
+        'dm_location': 'The treasure is in Baba Lysaga’s hut (chapter 10, area U3).'},
+    21:{
+        'name': 'Rogue', 
+        'represents':'Anyone for whom money is important—those who believe money is the key to their success',
+        'pred_location':'I see a nest of ravens. There you will find the prize.',
+        'dm_location': 'The treasure is hidden in the attic of the Blue Water Inn (chapter 5, area N2q).'},
+    22:{
+        'name': 'Swashbuckler', 
+        'represents':'Those who like money yet give it up freely—likable rogues and rapscallions',
+        'pred_location':'I see the skeleton of a deadly warrior, lying on a bed of stone flanked by gargoyles.',
+        'dm_location': 'The treasure lies in the crypt of Endorovich (chapter 4, area K84, crypt 7).'},
+    23:{
+        'name': 'Philanthropist', 
+        'represents':'Charity and giving on a grand scale—those who use wealth to fight evil and sickness',
+        'pred_location':'Look to a place where sickness and madness are bred. Where children once cried, the treasure lies still.',
+        'dm_location': 'The treasure is in the nursery of the Abbey of Saint Markovia (chapter 8, area S23).'},
+    24:{
+        'name': 'Trader', 
+        'represents':'Commerce—smuggling and black markets—fair and equitable trades',
+        'pred_location':'Look to the wizard of wines! In wood and sand the treasure hides.',
+        'dm_location': 'The treasure lies in the glassblower’s workshop in the Wizard of Wines winery (chapter 12, area W10).'},
+    25:{
+        'name': 'Merchant', 
+        'represents':'A rare commodity or business opportunity—deceitful or dangerous business transactions',
+        'pred_location':'Seek a cask that once contained the finest wine, of which not a drop remains.',
+        'dm_location': 'The treasure lies in Castle Ravenloft’s wine cellar (chapter 4, area K63).'},
+    26:{
+        'name': 'Guild Member', 
+        'represents':'Like-minded individuals joined together in a common goal—pride in one\'s work',
+        'pred_location':'I see a dark room full of bottles. It is the tomb of a guild member.',
+        'dm_location': 'The treasure lies in the crypt of Artank Swilovich (chapter 4, area K84, crypt 5).'},
+    27:{
+        'name': 'Beggar', 
+        'represents':'Sudden change in economic status or fortune',
+        'pred_location':'A wounded elf has what you seek. He will part with the treasure to see his dark dreams fulfilled.',
+        'dm_location': 'The treasure is hidden in Kasimir’s hovel (chapter 5, area N9a).'},
+    28:{
+        'name': 'Thief', 
+        'represents':'Those who steal or burgle—a loss of property, beauty, innocence, friendship, or reputation',
+        'pred_location':'What you seek lies at the crossroads of life and death, among the buried dead.',
+        'dm_location': 'The treasure is buried in the graveyard at the River Ivlis crossroads (chapter 2, area F).'},
+    29:{
+        'name': 'Tax Collector', 
+        'represents':'Corruption—honesty in an otherwise corrupt government or organization',
+        'pred_location':'The Vistani have what you seek. A missing child holds the key to the treasure’s release.',
+        'dm_location': 'The treasure is hidden in the Vistani treasure wagon (chapter 5, area N9i). “A missing child” refers to Arabelle (see chapter 2, area L).'},
+    30:{
+        'name': 'Miser', 
+        'represents':'Hoarded wealth—those who are irreversibly unhappy or who think money is meaningless',
+        'pred_location':'Look for a fortress inside a fortress, in a place hidden behind fire.',
+        'dm_location': 'The treasure lies in Castle Ravenloft’s treasury (chapter 4, area K41).'},
+    31:{
+        'name': 'Priest', 
+        'represents':'Enlightenment—those who follow a deity, a system of values, or a higher purpose',
+        'pred_location':'You will find what you seek in the castle, amid the ruins of a place of supplication.',
+        'dm_location': 'The treasure lies in Castle Ravenloft’s chapel (chapter 4, area K15).'},
+    32:{
+        'name': 'Monk', 
+        'represents':'Serenity—inner strength and self-reliance—supreme confidence bereft of arrogance',
+        'pred_location':'The treasure you seek is hidden behind the sun, in the house of a saint.',
+        'dm_location': 'The treasure lies in the main hall of the Abbey of Saint Markovia (chapter 8, area S13).'},
+    33:{
+        'name': 'Missionary', 
+        'represents':'Those who spread wisdom and faith to others—warnings of the spread of fear and ignorance',
+        'pred_location':'I see a garden dusted with snow, watched over by a scarecrow with a sackcloth grin. Look not to the garden but to the guardian.',
+        'dm_location': 'The treasure is hidden inside one of the scarecrows in the garden of the Abbey of Saint Markovia (chapter 8, area S9).'},
+    34:{
+        'name': 'Healer', 
+        'represents':'Healing—a contagious illness, disease, or curse—those who practice the healing arts',
+        'pred_location':'Look to the west. Find a pool blessed by the light of the white sun.',
+        'dm_location': 'The treasure lies beneath the gazebo in the Shrine of the White Sun (chapter 8, area S4).'},
+    35:{
+        'name': 'Shepherd', 
+        'represents':'Those who protect others—one who bears a burden far too great to be shouldered alone',
+        'pred_location':'Find the mother—she who gave birth to evil.',
+        'dm_location': 'The treasure lies in the tomb of King Barov and Queen Ravenovia (chapter 4, area K88).'},
+    36:{
+        'name': 'Druid', 
+        'represents':'The ambivalence and cruelty of nature and those who feel drawn to it—inner turmoil',
+        'pred_location':'An evil tree grows atop a hill of graves where the ancient dead sleep. The ravens can help you find it. Look for the treasure there.',
+        'dm_location': 'The treasure lies at the base of the Gulthias tree (chapter 14, area Y4). Any wereraven encountered in the wilderness can lead the characters to the location.'},
+    37:{
+        'name': 'Anarchist', 
+        'represents':'A fundamental change brought on by one whose beliefs are being put to the test',
+        'pred_location':'I see walls of bones, a chandelier of bones, and a table of bones—all that remains of enemies long forgotten.',
+        'dm_location': 'The treasure lies in Castle Ravenloft’s hall of bones (chapter 4, area K67).'},
+    38:{
+        'name': 'Charlatan', 
+        'represents':'Liars—those who profess to believe one thing but actually believe another',
+        'pred_location':'I see a lonely mill on a precipice. The treasure lies within.',
+        'dm_location': 'The treasure lies in the attic of Old Bonegrinder (chapter 6, area O4).'},
+    39:{
+        'name': 'Bishop', 
+        'represents':'Strict adherence to a code or a belief—those who plot, plan, and scheme',
+        'pred_location':'What you seek lies in a pile of treasure, beyond a set of amber doors.',
+        'dm_location': 'The treasure lies in the sealed treasury of the Amber Temple (chapter 13, area X40).'},
+    40:{
+        'name': 'Traitor', 
+        'represents':'Betrayal by someone close and trusted—a weakening or loss of faith',
+        'pred_location':'Look for a wealthy woman. A staunch ally of the devil, she keeps the treasure under lock and key, with the bones of an ancient enemy.',
+        'dm_location': 'The treasure is hidden in the master bedroom of Wachterhaus (chapter 5, area N4o).'},
+    41:{
+        'name': 'Abjurer', 
+        'represents':'Those guided by logic and reasoning—warns of an overlooked clue or piece of information',
+        'pred_location':'I see a fallen house guarded by a great stone dragon. Look to the highest peak.',
+        'dm_location':'The treasure lies in the beacon of Argynvostholt (chapter 7, area Q53). “Great stone dragon” refers to the statue in area Q1.'}}
