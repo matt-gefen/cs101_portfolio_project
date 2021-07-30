332834ef7641a9fe7ae50563d21a8ac5c321bbca
matt-gefen <matt.gefen@gmail.com>
2021-07-30 10:36:39 -0600

    adjusted common_deck numbering and added Drawing

--- common_deck.py
+++ common_deck.py
@@ -75,127 +75,127 @@ common_deck = {
         'represents':'The triumph of nature over civilization—natural disasters and bountiful harvests',
         'pred_location':'The treasure is hidden in a small castle beneath a mountain, guarded by amber giants.',
         'dm_location': 'The treasure is inside a model of Castle Ravenloft in the Amber Temple (chapter 13, area X20).'},
-    17:{
+    16:{
         'name': 'Evoker', 
         'represents':'Magical or supernatural power that can\'t be controlled—magic for destructive ends',
         'pred_location':'I see a kneeling woman—a rose of great beauty plucked too soon. The master of the marsh knows of whom I speak.',
         'dm_location': 'The treasure lies under Marina’s monument in Berez (chapter 10, area U5). “The master of the marsh” refers to Burgomaster Lazlo Ulrich (area U2), whose ghost can point characters toward the monument.'},
-    18:{
+    17:{
         'name': 'Illusionist', 
         'represents':'Lies and deceit—grand conspiracies—secret societies—the presence of a dupe or a saboteur',
         'pred_location':'A man is not what he seems. He comes here in a carnival wagon. Therein lies what you seek.',
         'dm_location': 'The treasure lies in Rictavio’s carnival wagon (chapter 5, area N5).'},
-    19:{
+    18:{
         'name': 'Necromancer', 
         'represents':'Unnatural events and unhealthy obsessions—those who follow a destructive path',
         'pred_location':'A woman hangs above a roaring fire. Find her, and you will find the treasure.',
         'dm_location': 'The treasure lies in Castle Ravenloft’s study (chapter 4, area K37).'},
-    20:{
+    19:{
         'name': 'Conjurer', 
         'represents':'The coming of an unexpected supernatural threat—those who think of themselves as gods',
         'pred_location':'I see a dead village, drowned by a river, ruled by one who has brought great evil into the world.',
         'dm_location': 'The treasure is in Baba Lysaga’s hut (chapter 10, area U3).'},
-    21:{
+    20:{
         'name': 'Rogue', 
         'represents':'Anyone for whom money is important—those who believe money is the key to their success',
         'pred_location':'I see a nest of ravens. There you will find the prize.',
         'dm_location': 'The treasure is hidden in the attic of the Blue Water Inn (chapter 5, area N2q).'},
-    22:{
+    21:{
         'name': 'Swashbuckler', 
         'represents':'Those who like money yet give it up freely—likable rogues and rapscallions',
         'pred_location':'I see the skeleton of a deadly warrior, lying on a bed of stone flanked by gargoyles.',
         'dm_location': 'The treasure lies in the crypt of Endorovich (chapter 4, area K84, crypt 7).'},
-    23:{
+    22:{
         'name': 'Philanthropist', 
         'represents':'Charity and giving on a grand scale—those who use wealth to fight evil and sickness',
         'pred_location':'Look to a place where sickness and madness are bred. Where children once cried, the treasure lies still.',
         'dm_location': 'The treasure is in the nursery of the Abbey of Saint Markovia (chapter 8, area S23).'},
-    24:{
+    23:{
         'name': 'Trader', 
         'represents':'Commerce—smuggling and black markets—fair and equitable trades',
         'pred_location':'Look to the wizard of wines! In wood and sand the treasure hides.',
         'dm_location': 'The treasure lies in the glassblower’s workshop in the Wizard of Wines winery (chapter 12, area W10).'},
-    25:{
+    24:{
         'name': 'Merchant', 
         'represents':'A rare commodity or business opportunity—deceitful or dangerous business transactions',
         'pred_location':'Seek a cask that once contained the finest wine, of which not a drop remains.',
         'dm_location': 'The treasure lies in Castle Ravenloft’s wine cellar (chapter 4, area K63).'},
-    26:{
+    25:{
         'name': 'Guild Member', 
         'represents':'Like-minded individuals joined together in a common goal—pride in one\'s work',
         'pred_location':'I see a dark room full of bottles. It is the tomb of a guild member.',
         'dm_location': 'The treasure lies in the crypt of Artank Swilovich (chapter 4, area K84, crypt 5).'},
-    27:{
+    26:{
         'name': 'Beggar', 
         'represents':'Sudden change in economic status or fortune',
         'pred_location':'A wounded elf has what you seek. He will part with the treasure to see his dark dreams fulfilled.',
         'dm_location': 'The treasure is hidden in Kasimir’s hovel (chapter 5, area N9a).'},
-    28:{
+    27:{
         'name': 'Thief', 
         'represents':'Those who steal or burgle—a loss of property, beauty, innocence, friendship, or reputation',
         'pred_location':'What you seek lies at the crossroads of life and death, among the buried dead.',
         'dm_location': 'The treasure is buried in the graveyard at the River Ivlis crossroads (chapter 2, area F).'},
-    29:{
+    28:{
         'name': 'Tax Collector', 
         'represents':'Corruption—honesty in an otherwise corrupt government or organization',
         'pred_location':'The Vistani have what you seek. A missing child holds the key to the treasure’s release.',
         'dm_location': 'The treasure is hidden in the Vistani treasure wagon (chapter 5, area N9i). “A missing child” refers to Arabelle (see chapter 2, area L).'},
-    30:{
+    29:{
         'name': 'Miser', 
         'represents':'Hoarded wealth—those who are irreversibly unhappy or who think money is meaningless',
         'pred_location':'Look for a fortress inside a fortress, in a place hidden behind fire.',
         'dm_location': 'The treasure lies in Castle Ravenloft’s treasury (chapter 4, area K41).'},
-    31:{
+    30:{
         'name': 'Priest', 
         'represents':'Enlightenment—those who follow a deity, a system of values, or a higher purpose',
         'pred_location':'You will find what you seek in the castle, amid the ruins of a place of supplication.',
         'dm_location': 'The treasure lies in Castle Ravenloft’s chapel (chapter 4, area K15).'},
-    32:{
+    31:{
         'name': 'Monk', 
         'represents':'Serenity—inner strength and self-reliance—supreme confidence bereft of arrogance',
         'pred_location':'The treasure you seek is hidden behind the sun, in the house of a saint.',
         'dm_location': 'The treasure lies in the main hall of the Abbey of Saint Markovia (chapter 8, area S13).'},
-    33:{
+    32:{
         'name': 'Missionary', 
         'represents':'Those who spread wisdom and faith to others—warnings of the spread of fear and ignorance',
         'pred_location':'I see a garden dusted with snow, watched over by a scarecrow with a sackcloth grin. Look not to the garden but to the guardian.',
         'dm_location': 'The treasure is hidden inside one of the scarecrows in the garden of the Abbey of Saint Markovia (chapter 8, area S9).'},
-    34:{
+    33:{
         'name': 'Healer', 
         'represents':'Healing—a contagious illness, disease, or curse—those who practice the healing arts',
         'pred_location':'Look to the west. Find a pool blessed by the light of the white sun.',
         'dm_location': 'The treasure lies beneath the gazebo in the Shrine of the White Sun (chapter 8, area S4).'},
-    35:{
+    34:{
         'name': 'Shepherd', 
         'represents':'Those who protect others—one who bears a burden far too great to be shouldered alone',
         'pred_location':'Find the mother—she who gave birth to evil.',
         'dm_location': 'The treasure lies in the tomb of King Barov and Queen Ravenovia (chapter 4, area K88).'},
-    36:{
+    35:{
         'name': 'Druid', 
         'represents':'The ambivalence and cruelty of nature and those who feel drawn to it—inner turmoil',
         'pred_location':'An evil tree grows atop a hill of graves where the ancient dead sleep. The ravens can help you find it. Look for the treasure there.',
         'dm_location': 'The treasure lies at the base of the Gulthias tree (chapter 14, area Y4). Any wereraven encountered in the wilderness can lead the characters to the location.'},
-    37:{
+    36:{
         'name': 'Anarchist', 
         'represents':'A fundamental change brought on by one whose beliefs are being put to the test',
         'pred_location':'I see walls of bones, a chandelier of bones, and a table of bones—all that remains of enemies long forgotten.',
         'dm_location': 'The treasure lies in Castle Ravenloft’s hall of bones (chapter 4, area K67).'},
-    38:{
+    37:{
         'name': 'Charlatan', 
         'represents':'Liars—those who profess to believe one thing but actually believe another',
         'pred_location':'I see a lonely mill on a precipice. The treasure lies within.',
         'dm_location': 'The treasure lies in the attic of Old Bonegrinder (chapter 6, area O4).'},
-    39:{
+    38:{
         'name': 'Bishop', 
         'represents':'Strict adherence to a code or a belief—those who plot, plan, and scheme',
         'pred_location':'What you seek lies in a pile of treasure, beyond a set of amber doors.',
         'dm_location': 'The treasure lies in the sealed treasury of the Amber Temple (chapter 13, area X40).'},
-    40:{
+    39:{
         'name': 'Traitor', 
         'represents':'Betrayal by someone close and trusted—a weakening or loss of faith',
         'pred_location':'Look for a wealthy woman. A staunch ally of the devil, she keeps the treasure under lock and key, with the bones of an ancient enemy.',
         'dm_location': 'The treasure is hidden in the master bedroom of Wachterhaus (chapter 5, area N4o).'},
-    41:{
+    40:{
         'name': 'Abjurer', 
         'represents':'Those guided by logic and reasoning—warns of an overlooked clue or piece of information',
         'pred_location':'I see a fallen house guarded by a great stone dragon. Look to the highest peak.',
--- tarokka.py
+++ tarokka.py
@@ -1,13 +1,39 @@
-import common_deck
-import high_deck
+from common_deck import common_deck
+from high_deck import high_deck
+import random
+
+print(len(common_deck))
+
+class Drawing:
+    def __init__(self,reading_num):
+        self.reading_num = reading_num
+        self.drawings = {
+            1:{'reading': 'This card tells of history. Knowledge of the ancient will help you better understand your enemy.', 'name': 'The Tome of  Strahd'},
+            2:{'reading': 'This card tells of a powerful force for good and protection, a holy symbol of great hope.','name': 'The Holy Symbol of   Ravenkind'},
+            3:{'reading': 'This is a card of power and strength. It tells of a weapon of vengeance: a sword of sunlight.','name':'The Sunsword'},
+            4:{'reading': 'This card sheds light on one who will help you greatly in the battle against darkness.','name': 'Strahd\'s Enemy'},
+            5:{'reading':'Your enemy is a creature of darkness, whose powers are beyond mortality. This card will lead you to him!','name':'Strahd\'s Ravenloft Location'}}
+
+    def reading(self):
+        return self.drawings[self.reading_num]['reading']
+
+    # def prediction(self):
+    #     prediction = ''
+    #     common_deck_len = len(common_deck)
+    #     if self.reading_num < 4:
+    #         random_drawing = random.randrange()
 
 
-drawings= {
-    1:{'reading': 'This card tells of history. Knowledge of the ancient will help you better understand your enemy.', 'name': 'The Tome of Strahd'},
-    2:{'reading': 'This card tells of a powerful force for good and protection, a holy symbol of great hope.','name': 'The Holy Symbol of Ravenkind'},
-    3:{'reading': 'This is a card of power and strength. It tells of a weapon of vengeance: a sword of sunlight.','name':'The Sunsword'},
-    4:{'reading': 'This card sheds light on one who will help you greatly in the battle against darkness.','name': 'Strahd\'s Enemy'},
-    5:{'reading':'Your enemy is a creature of darkness, whose powers are beyond mortality. This card will lead you to him!','name':'Strahd\'s Ravenloft pred_location'}}
 
+
+test_drawing = Drawing(1)
+print(test_drawing.reading())
+
+def initiate_reading():
+    print('\nMagic flames cast a reddish glow over the interior of this tent, revealing a low table covered in a black velvet cloth. Glints of light seem to flash from a crystal ball on the table as a hunched figure peers into its depths. As the crone speaks, her voice crackles like dry weeds. "At last you have arrived!" Cackling laughter bursts like mad lightning from her withered lips.\n')
+
+    print('I am Madame Eva, and I sense your threads of your destinies. Tell me, outsiders...What are your names?\n')
+    name = input()
+    print('\nHello {}\n'.format(name))
 
 
